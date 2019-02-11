a24e18d858761e42427c2419e915d66ba4ae02ad
hocnguyen <hocnguyen@chotot.vn>
2019-02-11 19:50:53 +0700

    remove node_modules

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 run_dir=$(pwd)
 npm config set registry http://npm.chotot.org
-cd packages/platform-modules && npm install --production && node bundle.js
+cd /home/meteor/platform-modules && npm install --production && node bundle.js
+ln -s /home/meteor/platform-modules $run_dir/packages
 cd $run_dir
 meteor npm install --production
 meteor build /home/meteor/bundle --server-only --directory --allow-incompatible-update
