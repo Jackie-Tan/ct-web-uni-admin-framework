5db4dbf326b31c8de5799b6cf082b5ac475179cf
hocnguyen <hocnguyen@chotot.vn>
2019-02-11 22:05:49 +0700

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 run_dir=$(pwd)
-cd /home/meteor/platform-modules && npm install --production && node bundle.js && rm -rf node_modules
-cp -R /home/meteor/platform-modules $run_dir/packages/platform-modules
+cd $run_dir/packages/platform-modules && npm install --production && node bundle.js
 cd $run_dir
 meteor npm install --production
 meteor build /home/meteor/bundle --server-only --directory --allow-incompatible-update
