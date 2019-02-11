5ed3b3ae99590c4f6f0c7664b1787a21c433dfe3
hocnguyen <hocnguyen@chotot.vn>
2019-02-11 20:21:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 run_dir=$(pwd)
-npm config set registry http://npm.chotot.org
-cd /home/meteor/platform-modules && npm install --production && node bundle.js
-ln -s /home/meteor/platform-modules $run_dir/packages
+cd /home/meteor/platform-modules && npm install --production && node bundle.js && rm -rf node_modules
+cp -R /home/meteor/platform-modules $run_dir/packages/platform-modules
 cd $run_dir
 meteor npm install --production
 meteor build /home/meteor/bundle --server-only --directory --allow-incompatible-update
