db1eda6c1208311a9d51bb4d88dedcdba7c02a03
hongnguyen <hongnguyen@chotot.vn>
2019-04-23 13:47:59 +0700

    chotot-google: env to disable_oauth

--- packages/chotot-google/init.js
+++ packages/chotot-google/init.js
@@ -1,25 +1,27 @@
 ServiceConfiguration.configurations.remove({
-  service: "google"
+    service: "google"
 });
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: process.env.APP_GOOGLE_CLIENT_ID,
-  secret: process.env.APP_GOOGLE_SECRET
-});
-if (!Accounts.serviceMap) {
-  Accounts.serviceMap = {}
+
+if (!process.env.DISABLE_OAUTH) {
+    ServiceConfiguration.configurations.insert({
+        service: "google",
+        clientId: process.env.APP_GOOGLE_CLIENT_ID,
+        secret: process.env.APP_GOOGLE_SECRET
+    });
+    if (!Accounts.serviceMap) {
+        Accounts.serviceMap = {}
+    }
+    Accounts.serviceMap.google = function (options, user) {
+        let sv = user.services
+        let email = sv.google.email, username = sv.google.email
+        let emailSp = email.split('@')
+        if (emailSp[1] != 'chotot.vn')
+            throw new Meteor.Error(
+                new Accounts.LoginCancelledError("ERROR_AUTH_LOGIN"),
+                "Không hổ trợ email ngoài Chợ Tốt!")
+        user.username = username;
+        user.emails = [{
+            address: email
+        }];
+    }
 }
-Accounts.serviceMap.google = function (options, user) {
-  let sv = user.services
-  let email= sv.google.email, username = sv.google.email
-  let emailSp = email.split('@')
-  if (emailSp[1] != 'chotot.vn')
-    throw new Meteor.Error(
-        new Accounts.LoginCancelledError("ERROR_AUTH_LOGIN"),
-        "Không hổ trợ email ngoài Chợ Tốt!")
-  user.username = username;
-  user.emails = [{
-    address: email
-  }];
-}
-  // return true
--- packages/chotot-google/package.js
+++ packages/chotot-google/package.js
@@ -1,20 +1,22 @@
 Package.describe({
-  summary: "Login service for google accounts",
-  version: "1.2.0",
-  name: "chotot:accounts-google"
+    summary: "Login service for google accounts",
+    version: "1.2.0",
+    name: "chotot:accounts-google"
 });
 
-Package.onUse(function(api) {
-  api.use(['ecmascript','accounts-google']);
-  api.use([
-    'chotot:logger',
-  ], ['server']);
-  api.imply([
-    'useraccounts:core@1.14.2',
-  ], ['client']);
-  api.use('service-configuration', 'server');
-  api.use('accounts-base', ['client', 'server']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.addFiles('init.js', 'server')
-});
+if (!process.env.DISABLE_OAUTH) {
+    Package.onUse(function (api) {
+        api.use(['ecmascript', 'accounts-google']);
+        api.use([
+            'chotot:logger',
+        ], ['server']);
+        api.imply([
+            'useraccounts:core@1.14.2',
+        ], ['client']);
+        api.use('service-configuration', 'server');
+        api.use('accounts-base', ['client', 'server']);
+        // Export Accounts (etc) to packages using this one.
+        api.imply('accounts-base', ['client', 'server']);
+        api.addFiles('init.js', 'server')
+    });
+}
