8f9b63aa38f6c6d62cb5bf2c7082219e389b8124
huy nguyen <quochuy@chotot.vn>
2019-05-20 16:38:47 +0700

    try add graylog level

--- packages/chotot-logger/logger.js
+++ packages/chotot-logger/logger.js
@@ -3,12 +3,15 @@ const MAP_LOG_LEVEL = {
   "ACTION": 1,
   "DEBUG": 2,
   "TRACE": 3,
+  "GRAYLOG": 4
 }
 var moment = require('moment-timezone');
 const statClient = Npm.require('prom-client');
+
 const buckets = [
   0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.15, 0.25, 0.4, 0.6, 0.8, 1, 1.5, 2, 3, 5
-]
+];
+
 const statH = new statClient.Histogram(
   'request_duration_seconds',
   'The HTTP request latencies in seconds.',
@@ -16,26 +19,29 @@ const statH = new statClient.Histogram(
   {
     buckets: buckets
   }
-)
+);
+
 var isNotLog = function (debugLevel) {
   let level = process.env.LOG_LEVEL;
-  if (!level || typeof level != 'string')
+  if (!level || typeof level != 'string') {
     return true;
+  }
   return MAP_LOG_LEVEL[level.toUpperCase()] < MAP_LOG_LEVEL[debugLevel.toUpperCase()];
-
 }
+
 logger = {
   send: function(data) {
     console.log('xx')
   },
   action: function (action, ip, data, where) {
-    if (isNotLog('action'))
-      return
-    let time = moment().tz("Asia/Ho_Chi_Minh").format("YYYY/MM/DD HH:mm:ss")
-    let user = Meteor.user()
-    let msgUser = user?`${ip} ${user.username || user.emails[0].address} ${user._id}`:"system.."
-    let msgWhere = where? `[${where}]`:""
-    console.log(`[${time}]${msgWhere} ${msgUser} ${action} with args`, JSON.stringify(data))
+    if (isNotLog('action')) {
+      return;
+    }
+    let time = moment().tz("Asia/Ho_Chi_Minh").format("YYYY/MM/DD HH:mm:ss");
+    let user = Meteor.user();
+    let msgUser = user ? `${ip} ${user.username || user.emails[0].address} ${user._id}` : "system..";
+    let msgWhere = where ? `[${where}]` : "";
+    console.log(`[${time}] ${msgWhere} ${msgUser} ${action} with args`, JSON.stringify(data));
   },
   metrics: function() {
     return statClient.register.metrics();
@@ -47,45 +53,59 @@ logger = {
     logger.info(status, path, 'Duration_request:', duration, '_end_duration');
   },
   time: function (data) {
-    let time = new Date().getTime()
-    console.log(`${time} `)
+    let time = new Date().getTime();
+    console.log(`${time} `);
   },
   info: function () {
-    console.log.apply(null, arguments)
+    console.log.apply(null, arguments);
   },
   debug: function () {
-    return isNotLog('debug') || console.log.apply(null, arguments)
+    return isNotLog('debug') || console.log.apply(null, arguments);
   },
   trace: function () {
-    return isNotLog('trace') || console.trace.apply(null, arguments)
+    return isNotLog('trace') || console.trace.apply(null, arguments);
   },
   error: function () {
-    return console.error.apply(null, arguments)
+    return console.error.apply(null, arguments);
   },
   warn: function () {
-    return console.warn.apply(null, arguments)
+    return console.warn.apply(null, arguments);
   },
   bound: function (func, msg, errFunc) {
     try {
-      func.call()
+      func.call();
     } catch (e) {
       let newE = new Error(msg +' & '+ (e.message || e.reason));
       newE.stack = e.stack;
-      if (!errFunc || typeof errFunc != 'function')
+      if (!errFunc || typeof errFunc != 'function') {
         throw newE;
+      }
       errFunc(newE)
     }
+  },
+  graylog: function() {
+    return console.log({
+      "version": "1.1",
+      "host": "example.org",
+      "short_message": "A short message that helps you identify what is going on",
+      "full_message": "Backtrace here\n\nmore stuff",
+      "timestamp": 1385053862.3072,
+      "level": 1,
+      "_user_id": 9001,
+      "_some_info": "foo",
+      "_some_env_var": "bar"
+    });
   }
 }
 
 Meteor.methods({
-  'Global/Debug':function(level){
+  'Global/Debug': function(level) {
     level = ""+level;
     if (typeof MAP_LOG_LEVEL[level.toUpperCase()] == 'undefined') {
-      logger.info('We do not have this log level')
-      throw  new Meteor.Error('We do not have this log level!')
+      logger.info('We do not have this log level');
+      throw  new Meteor.Error('We do not have this log level!');
     }
-    logger.info('Set log level to: ', level)
+    logger.info('Set log level to: ', level);
     process.env.LOG_LEVEL = level;
   }
 })
--- packages/chotot-platform/lib/client/index.js
+++ packages/chotot-platform/lib/client/index.js
@@ -1,24 +1,29 @@
 class Client {
-    constructor(url, api_env){
-      if (Meteor.isClient)
-        this.api_url =""
-      else
-        this.api_url = process.env[api_env] || process.env.GATEWAY_URL || 'https://gateway.chotot.org'
-      this.opt = {}
+    constructor(url, api_env) {
+      if (Meteor.isClient) {
+        this.api_url = "";
+      } else {
+        this.api_url = process.env[api_env] || process.env.GATEWAY_URL || 'https://gateway.chotot.org';
+      }
+      this.opt = {};
       this.api_url = ("http://"+this.api_url).replace(/http[^s]*(s*)[^\/]\/\//, "http$1://");
       this.url = this.api_url + url;
     }
+
     static of(url, api_env){
       return new Client(url, api_env)
     }
+
     headers(headers) {
       this.opt.headers = headers;
       return this;
     }
+
     data(data){
       this.opt.data = data;
       return this;
     }
+
     exec(method, refactor) {
       const self = this;
       return new Promise((resolve, reject) => {
@@ -40,10 +45,9 @@ class Client {
           }
           return resolve(data);
         }))
-  
+
       })
     }
   }
-  
+
   module.exports=Client
-  
