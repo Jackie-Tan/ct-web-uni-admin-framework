4f860c62c1dc0f5c6e16f303b59647c9c1cb35e6
huy nguyen <quochuy@chotot.vn>
2019-05-20 17:00:36 +0700

    try log

--- packages/chotot-logger/logger.js
+++ packages/chotot-logger/logger.js
@@ -57,7 +57,17 @@ logger = {
     console.log(`${time} `);
   },
   info: function () {
-    console.log.apply(null, arguments);
+    console.log.apply({
+      "version": "1.1",
+      "host": "example.org",
+      "short_message": "A short message that helps you identify what is going on",
+      "full_message": "Backtrace here\n\nmore stuff",
+      "timestamp": 1385053862.3072,
+      "level": 1,
+      "_user_id": 9001,
+      "_some_info": "foo",
+      "_some_env_var": "bar"
+    });
   },
   debug: function () {
     return isNotLog('debug') || console.log.apply(null, arguments);
