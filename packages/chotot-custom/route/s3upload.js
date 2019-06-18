604fcaa124a84b0288a8b6378d92e8eba0837de4
hongnguyen <hongnguyen@chotot.vn>
2019-06-18 22:18:58 +0700

    feat(s3Upload): route to upload without renaming the image

--- packages/chotot-custom/route/s3upload.js
+++ packages/chotot-custom/route/s3upload.js
@@ -6,36 +6,49 @@ const STATIC_URL = process.env.S3_STATIC_URL || 'https://static.chotot.com.vn/st
 const PATH = '/' + (process.env.S3_BUCKET || 'admin-centre');
 const encrypt = require('./lib/encrypt')
 import Request from 'request';
-const FWRequrest = function (length =2) {
+const FWRequrest = function (length = 2) {
+  var request = this.request;
+  //TODO refactor headers
+  var FOLDER = '/' + request.originalUrl.split('/').slice(-length).join('_');
+  let file_name = `${FOLDER}_${new Date().getTime()}.jpg`
+  UploadS3.call(this, FOLDER, file_name)
+}
+
+const UploadS3 = function (FOLDER, fileName) {
   var request = this.request;
   var response = this.response;
   //TODO refactor headers
+  let time = (new Date()).toUTCString()
+  let s = `PUT\n\nimage/jpeg\n\nx-amz-acl:public-read\nx-amz-date:${time}\n${PATH + FOLDER + fileName}`;
   let headers = {};
-  var FOLDER = '/'+request.originalUrl.split('/').slice(-length).join('_');
-  let time =(new Date()).toUTCString()
-  let file_name = `${FOLDER}_${new Date().getTime()}.jpg`
-  let s = `PUT\n\nimage/jpeg\n\nx-amz-acl:public-read\nx-amz-date:${time}\n${PATH+FOLDER+file_name}`;
   headers['content-type'] = 'image/jpeg'
   headers['x-amz-date'] = time;
   headers['x-amz-acl'] = 'public-read'
-  headers['Authorization'] = "AWS " + ACCESS_KEY + ":" + encrypt.b64_hmac_sha1(SECRET_KEY,s);
-  var url = DOMAIN+PATH+FOLDER+file_name
-  var chototUploadImage = request.pipe(Request.put({method:'PUT', uri: url, headers: headers}))
-  // console.log(process.env.IMAGINARY_UPLOAD+'/'+image_id);
+  headers['Authorization'] = "AWS " + ACCESS_KEY + ":" + encrypt.b64_hmac_sha1(SECRET_KEY, s);
+  var url = DOMAIN + PATH + FOLDER + fileName
+  var chototUploadImage = request.pipe(Request.put({ method: 'PUT', uri: url, headers: headers }))
   chototUploadImage.on('response', function (resp) {
-      response.writeHead(200, {"Content-Type": "application/json"});
-      response.end(JSON.stringify({url: STATIC_URL + PATH + FOLDER + file_name}))
-  }).on('error', function(err) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ url: STATIC_URL + PATH + FOLDER + fileName }))
+  }).on('error', function (err) {
     console.error(err)
-    response.writeHead(400, {"Content-Type": "application/json"});
-    response.end(JSON.stringify({error: err.message}))
+    response.writeHead(400, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ error: err.message }))
   });
 }
-Router.route('/s3-upload/:folder', {where: 'server'}).put(function () {
+
+Router.route('/s3-upload/:folder', { where: 'server' }).put(function () {
   FWRequrest.call(this, 1);
 })
 
-Router.route('/s3-upload/:prefix/:folder', {where: 'server'}).put(function () {
+Router.route('/s3-upload/:prefix/:folder', { where: 'server' }).put(function () {
   FWRequrest.call(this, 2);
 })
 
+Router.route('/s3-upload-keep-file-name/:folder/:file_name', { where: 'server' }).put(function () {
+  let folder = this.params.folder
+  let fileName = this.params.file_name
+  if (folder && fileName) {
+    UploadS3.call(this, `/${folder}`, `/${fileName}`);
+  }
+})
