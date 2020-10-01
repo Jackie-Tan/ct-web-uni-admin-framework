eab476513ae61993a5caac7ae9d7bf11384239e6
Lam Duy Lac <laclam@chotot.vn>
2020-10-01 16:24:33 +0700

    Add new field

--- client/input/datetimevn/input.js
+++ client/input/datetimevn/input.js
@@ -6,12 +6,19 @@ Template.datetimevnInput.onRendered(function(){
     if (input.map)
       data.value = input.map(data.value)
     this.$('.datetimepickervn').datetimepicker({
-      format: 'MM/DD/YYYY HH:mm:ss.SSSSSS',
+      format: 'MM/DD/YYYY HH:mm:ss',
       defaultDate: new Date(this.data.value),
       timeZone: "+7"
     });
   }
   else {
-    this.$('.datetimepickervn').datetimepicker();
+    let format = 'MM/DD/YY 00:00:00'
+    if (this.$('.datetimepickervn').attr('isEndOfDay') === "true") {
+      format = 'MM/DD/YY 23:59:59'
+    }
+    this.$('.datetimepickervn').datetimepicker({
+      format,
+      timeZone: "+7"
+    });
   }
 })
