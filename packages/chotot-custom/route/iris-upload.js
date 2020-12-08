c31318a9fed5aebcd46efa7e78f31bb69d378af2
Toan Nguyen <toannguyen@chotot.vn>
2020-12-09 06:27:38 +0700

    Fix: register iris upload to root file

---
+++ packages/chotot-custom/route/iris-upload.js
@@ -0,0 +1,10 @@
+const { Uploader } = require('ct-iris-client');
+
+const URL_UPLOAD = process.env.IRIS_URL || 'https://gateway.chotot.org/v1/internal/images/upload'
+
+Router.route('/iris/image-upload', {where: 'server'}).post(function () {
+  var request = this.request;
+  var response = this.response;
+  const uploader = new Uploader(URL_UPLOAD);
+  console.log('request', request);
+});
