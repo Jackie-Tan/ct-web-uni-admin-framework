b2675c3e0f9b3f61dd48f40c9372b87d9d3e43e8
Toan Nguyen <toannguyen@chotot.vn>
2020-12-15 06:39:07 +0700

    Fix: update packagage

--- packages/chotot-custom/route/iris-upload.js
+++ packages/chotot-custom/route/iris-upload.js
@@ -8,33 +8,40 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
   var response = this.response;
   const readable = new Readable();
   readable._read = () => {};
+  let body = [];
   request
+    .on('error', (err) => {
+      console.error(err);
+    })
     .on('data', data => {
-      readable.push(data);
+      // readable.push(data);
+      body.push(data);
     })
     .on('end', () => {
-      readable.push(null);
-      fetch(URL_UPLOAD, {
-        body: {
-          image: readable,
-          type: 'admincentre'
-        },
-        headers: {
-          "content-type": request.headers["content-type"],
-          "content-length": request.headers["content-length"],
-          "Tenant-Namespace": "chotot"
-        },
-        method: "POST",
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          response.writeHead(200, { "Content-Type": "application/json" });
-          response.end(JSON.stringify(res));
-        })
-        .catch((err) => {
-          response.writeHead(400, { "Content-Type": "application/json" });
-          response.end(JSON.stringify({ error: err.message }));
-        });
+      // readable.push(null);
+      body = Buffer.concat(body).toString();
+      console.log('body', body);
+      // fetch(URL_UPLOAD, {
+      //   body: {
+      //     image: readable,
+      //     type: 'admincentre'
+      //   },
+      //   headers: {
+      //     "content-type": request.headers["content-type"],
+      //     "content-length": request.headers["content-length"],
+      //     "Tenant-Namespace": "chotot"
+      //   },
+      //   method: "POST",
+      // })
+      //   .then((res) => res.json())
+      //   .then((res) => {
+      //     response.writeHead(200, { "Content-Type": "application/json" });
+      //     response.end(JSON.stringify(res));
+      //   })
+      //   .catch((err) => {
+      //     response.writeHead(400, { "Content-Type": "application/json" });
+      //     response.end(JSON.stringify({ error: err.message }));
+      //   });
       console.log('end');
     });
 });
