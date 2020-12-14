73a375c55577f92070762442c3bfe8f464c7148d
Toan Nguyen <toannguyen@chotot.vn>
2020-12-14 15:49:10 +0700

    Fix: add iris upload

--- packages/chotot-custom/route/iris-upload.js
+++ packages/chotot-custom/route/iris-upload.js
@@ -1,4 +1,5 @@
 const { Uploader } = require('ct-iris-client');
+const Request = require('request');
 
 const URL_UPLOAD = process.env.IRIS_URL || 'https://gateway.chotot.org/v1/internal/images/upload'
 
@@ -6,5 +7,7 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
   var request = this.request;
   var response = this.response;
   const uploader = new Uploader(URL_UPLOAD);
-  console.log('request', request);
+  var chototUploadImage = Request.post(URL_UPLOAD)
+  request.pipe(chototUploadImage);
+  chototUploadImage.pipe(response);
 });
