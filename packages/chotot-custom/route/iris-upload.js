68a4b4ca4191bad73b161c4925bf966ff6affd4a
Toan Nguyen <toannguyen@chotot.vn>
2020-12-14 16:44:34 +0700

    Fix: update iris upload

--- packages/chotot-custom/route/iris-upload.js
+++ packages/chotot-custom/route/iris-upload.js
@@ -1,6 +1,6 @@
 const { Uploader } = require('ct-iris-client');
 const Request = require('request');
-// const { Readable } = "stream";
+const { Readable } = "stream";
 
 const URL_UPLOAD = process.env.IRIS_URL || 'https://gateway.chotot.org/v1/internal/images/upload'
 
@@ -8,32 +8,22 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
   var request = this.request;
   var response = this.response;
   const uploader = new Uploader(URL_UPLOAD);
-  const { image } = this.request.body;
-  console.log('this.request.query', this.request.body);
-  // var chototUploadImage = Request.post(URL_UPLOAD)
-  request.pipe(uploader.upload(image, { type: 'admincentre' }));
-  uploader.pipe(response);
-  // request.on("data", (data) => {
-  //   readable.push(data);
-  // })
-  // request.on("end", () => {
-  //   readable.push(null);
-  //   fetch(`${process.env.GATEWAY_URL}/v1/internal/images/upload`, {
-  //     body: readable,
-  //     headers: {
-  //       "content-type": request.headers["content-type"],
-  //       "content-length": request.headers["content-length"],
-  //     },
-  //     method: "POST",
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       response.writeHead(200, { "Content-Type": "application/json" });
-  //       response.end(JSON.stringify({ url: res.image_url }));
-  //     })
-  //     .catch((err) => {
-  //       response.writeHead(400, { "Content-Type": "application/json" });
-  //       response.end(JSON.stringify({ error: err.message }));
-  //     });
-  // });
+  const readable = new Readable();
+  readable._read = () => {};
+  request.on("data", (data) => {
+    readable.push(data);
+  })
+  request.on("end", () => {
+    readable.push(null);
+    uploader.upload(readable, { type: 'admincentre' })
+      .then((res) => res.json())
+      .then((res) => {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(res));
+      })
+      .catch((err) => {
+        response.writeHead(400, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(err));
+      });
+  });
 });
