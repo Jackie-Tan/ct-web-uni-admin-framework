164f0f73bad468025888b62ea3136e3964fda795
Toan Nguyen <toannguyen@chotot.vn>
2020-12-15 00:38:58 +0700

    Fix: update packagage

--- packages/chotot-custom/route/iris-upload.js
+++ packages/chotot-custom/route/iris-upload.js
@@ -1,39 +1,7 @@
-const Request = require('request');
-const fs = require('fs');
 const { Readable } = require('stream');
-const { Uploader } = require("ct-iris-client");
 const fetch = require('node-fetch');
 
 const URL_UPLOAD = process.env.IRIS_URL || 'https://gateway.chotot.org/v1/internal/images/upload'
-const uploader = new Uploader(URL_UPLOAD);
-
-const FWRequrest = function () {
-  // var request = this.request;
-  //TODO refactor headers
-  // let file_name = `${FOLDER}_${new Date().getTime()}.jpg`;
-  UploadIris.call(this);
-}
-
-const UploadIris = function () {
-  var request = this.request;
-  var response = this.response;
-  //TODO refactor headers
-  let headers = {};
-  headers['content-type'] = 'image/jpeg';
-  headers['Tenant-Namespace'] = 'chotot';
-  var chototUploadImage = Request.post(URL_UPLOAD);
-  request.pipe(chototUploadImage);
-  chototUploadImage.pipe(response);
-  // chototUploadImage.on('response', function (resp) {
-  //   console.log('resp', resp.body);
-  //   response.writeHead(200, { "Content-Type": "application/json" });
-  //   response.end(JSON.stringify(resp.body));
-  // }).on('error', function (err) {
-  //   console.error('err', err);
-  //   response.writeHead(400, { "Content-Type": "application/json" });
-  //   response.end(JSON.stringify({ error: err.message }));
-  // });
-}
 
 Router.route('/iris/image-upload', {where: 'server'}).post(function () {
   var request = this.request;
@@ -46,26 +14,22 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
     })
     .on('end', () => {
       readable.push(null);
-      // uploader
-      //   .upload(readable, { type: 'admincentre' })
-      //   .then(data => {
-      //     console.log('resp from iris', data);
-      //   })
-      //   .catch(error => {
-      //     console.log('error', error);
-      //   });
       fetch(URL_UPLOAD, {
-        body: readable,
+        body: {
+          image: readable,
+          type: 'admincentre'
+        },
         headers: {
           "content-type": request.headers["content-type"],
           "content-length": request.headers["content-length"],
+          "Tenant-Namespace": "chotot"
         },
         method: "POST",
       })
         .then((res) => res.json())
         .then((res) => {
           response.writeHead(200, { "Content-Type": "application/json" });
-          response.end(JSON.stringify({ url: res.image_url }));
+          response.end(JSON.stringify(res));
         })
         .catch((err) => {
           response.writeHead(400, { "Content-Type": "application/json" });
@@ -73,8 +37,4 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
         });
       console.log('end');
     });
-  // var chototUploadImage = Request.post(URL_UPLOAD);
-  // request.pipe(chototUploadImage);
-  // chototUploadImage.pipe(response);
-  // FWRequrest.call(this);
 });
