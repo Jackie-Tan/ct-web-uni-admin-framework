1f624069fc49bb86a43eadbea538a5e97c9401cb
Toan Nguyen <toannguyen@chotot.vn>
2020-12-15 09:43:15 +0700

    Fix: update packagage

--- packages/chotot-custom/route/iris-upload.js
+++ packages/chotot-custom/route/iris-upload.js
@@ -20,28 +20,24 @@ Router.route('/iris/image-upload', {where: 'server'}).post(function () {
     .on('end', () => {
       // readable.push(null);
       body = Buffer.concat(body).toString();
-      console.log('body', body);
-      // fetch(URL_UPLOAD, {
-      //   body: {
-      //     image: readable,
-      //     type: 'admincentre'
-      //   },
-      //   headers: {
-      //     "content-type": request.headers["content-type"],
-      //     "content-length": request.headers["content-length"],
-      //     "Tenant-Namespace": "chotot"
-      //   },
-      //   method: "POST",
-      // })
-      //   .then((res) => res.json())
-      //   .then((res) => {
-      //     response.writeHead(200, { "Content-Type": "application/json" });
-      //     response.end(JSON.stringify(res));
-      //   })
-      //   .catch((err) => {
-      //     response.writeHead(400, { "Content-Type": "application/json" });
-      //     response.end(JSON.stringify({ error: err.message }));
-      //   });
+      fetch(URL_UPLOAD, {
+        body: body,
+        headers: {
+          "content-type": request.headers["content-type"],
+          "content-length": request.headers["content-length"],
+          "Tenant-Namespace": "chotot"
+        },
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.end(JSON.stringify(res));
+        })
+        .catch((err) => {
+          response.writeHead(400, { "Content-Type": "application/json" });
+          response.end(JSON.stringify({ error: err.message }));
+        });
       console.log('end');
     });
 });
