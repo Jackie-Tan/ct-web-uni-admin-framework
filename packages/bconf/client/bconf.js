6e05ef12da9c905a9a38fac0a818f493db821e3d
Nguyen Tien Dat <datnguyen@chotot.vn>
2021-07-24 22:24:39 +0700

    init code to store bconf in indexed DB

--- packages/bconf/client/bconf.js
+++ packages/bconf/client/bconf.js
@@ -1,14 +1,39 @@
 const TIME_REFRESH = 1000 * 60 * 60;
 
+function initBconfDB() {
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
+
+  var openRequest = indexedDB.open('cp', 1);
+  openRequest.onupgradeneeded = (e) => {
+    let db = e.target.result;
+    db.objectStoreNames.contains("bconf") || db.createObjectStore('bconf', {
+      keyPath: 'name'
+    });
+  }
+}
+
 function getBconfD(cb) {
   Meteor.call('Global/Trans/Bconf', function (err, res) {
     if (err) {
       dsLog.error(err.message || err.reason);
       return;
     }
-    localStorage.setItem('BconfD', JSON.stringify(res))
-    Bconf.data = res;
-    cb && cb();
+    // localStorage.setItem('BconfD', JSON.stringify(res))
+    var openRequest = indexedDB.open('cp', 1)
+    openRequest.onsuccess = () => {
+      let store = openRequest.result.transaction("bconf", "readwrite").objectStore("bconf")
+      var item = {
+        name: 'BconfD',
+        data: JSON.stringify(res),
+        created: new Date().getTime()
+      };
+      store.put(item);
+      Bconf.data = res;
+      cb && cb();
+    }
   })
 }
 
@@ -19,10 +44,48 @@ function getBconfS(cb) {
       return;
     }
     delete res.data;
-    localStorage.setItem('BconfS', JSON.stringify(res))
-    _.extend(Bconf, res);
-    cb && cb();
+    // localStorage.setItem('BconfS', JSON.stringify(res))
+    var openRequest = indexedDB.open('cp', 1)
+    openRequest.onsuccess = () => {
+      let store = openRequest.result.transaction("bconf", "readwrite").objectStore("bconf");
+      var item = {
+        name: 'BconfS',
+        data: JSON.stringify(res),
+        created: new Date().getTime()
+      };
+      store.put(item);
+      _.extend(Bconf, res);
+      cb && cb();
+    }
   })
+}
+
+function getBconfValue(key, cb) {
+  var openRequest = indexedDB.open('cp', 1)
+  openRequest.onsuccess = () => {
+    let store = openRequest.result.transaction('bconf', 'readonly').objectStore("bconf");
+    let getRequest = store.get(key);
+    getRequest.onsuccess = function(event) {
+      if (getRequest.result != null) {
+        cb(getRequest.result.data);
+      } else {
+        cb(null);
+      }
+    };
+  }
+}
+
+function setBconfValue(key, val) {
+  var openRequest = indexedDB.open('cp', 1)
+  openRequest.onsuccess = () => {
+    let store = openRequest.result.transaction("bconf", "readwrite").objectStore("bconf");
+    var item = {
+      name: key,
+      data: val,
+      created: new Date().getTime()
+    };
+    store.put(item);
+  }
 }
 
 Bconf = {
@@ -33,34 +96,49 @@ Bconf = {
   data: null,
   dataS: null,
   init: function () {
-    Bconf.versions.bconf.set(localStorage.getItem('BconfDVersion'));
-    Bconf.versions.bconfS.set(localStorage.getItem('BconfSVersion'));
-    if (lbconfD = localStorage.getItem('BconfD')) {
-      Bconf.data = JSON.parse(lbconfD);
-    }
-    if (lbconfS = localStorage.getItem('BconfS')) {
-      _.extend(Bconf, JSON.parse(lbconfS));
-    }
-    console.log('---loading dynamic config----');
-    if (!Bconf.data) {
-      console.log('----from server----');
-      getBconfD();
-    }
-    console.log('---loading static config----');
-    if (!Bconf.dataS) {
-      console.log('----from server----');
-      getBconfS();
-    }
+    getBconfValue("BconfDVersion", function (data) {
+      Bconf.versions.bconf.set(data);
+    })
+
+    getBconfValue("BconfSVersion", function (data) {
+      Bconf.versions.bconfS.set(data);
+    })
+
+    getBconfValue("BconfD", function (data) {
+      if (data) {
+        console.log("BconfD data ");
+        Bconf.data = JSON.parse(data);
+      }
+      console.log('---loading dynamic config----');
+      if (!Bconf.data) {
+        console.log('----from server----');
+        getBconfD();
+      }
+    });
+
+    getBconfValue("BconfS", function (data) {
+      if (data) {
+        console.log("BconfS data ");
+        _.extend(Bconf, JSON.parse(data));
+      }
+      console.log('---loading static config----');
+      if (!Bconf.dataS) {
+        console.log('----from server----');
+        getBconfS();
+      }
+    });
   },
   refresh() {
     Tracker.autorun(function () {
       Meteor.subscribe('conf', function () {
-        let conf = Confs.find({id: 'bconf'}).fetch()[0];
+        let conf = Confs.find({
+          id: 'bconf'
+        }).fetch()[0];
         if (!conf)
           return;
         if (!Bconf.versions.bconf.curValue) {
-          Bconf.versions.bconf.set(conf.version)
-          localStorage.setItem('BconfDVersion', "" + conf.version);
+          Bconf.versions.bconf.set(conf.version);
+          setBconfValue("BconfDVersion", "" + conf.version)
           return;
         }
         if (Bconf.versions.bconf.curValue == conf.version) {
@@ -70,19 +148,21 @@ Bconf = {
         //dynamic bconf no need refresh
         getBconfD(function () {
           Bconf.versions.bconf.set(conf.version);
-          localStorage.setItem('BconfDVersion', "" + conf.version);
+          setBconfValue("BconfDVersion", "" + conf.version);
         });
         this.stop();
       });
     });
     Tracker.autorun(function () {
       Meteor.subscribe('conf', function () {
-        let conf = Confs.find({id: 'bconfS'}).fetch()[0];
+        let conf = Confs.find({
+          id: 'bconfS'
+        }).fetch()[0];
         if (!conf)
           return;
         if (!Bconf.versions.bconfS.curValue) {
-          Bconf.versions.bconfS.set(conf.version)
-          localStorage.setItem('BconfSVersion', "" + conf.version);
+          Bconf.versions.bconfS.set(conf.version);
+          setBconfValue("BconfSVersion", "" + conf.version);
           return;
         }
         if (Bconf.versions.bconfS.curValue == conf.version) {
@@ -91,7 +171,7 @@ Bconf = {
         console.log('---reloading static config----');
         getBconfS(function () {
           Bconf.versions.bconfS.set(conf.version);
-          localStorage.setItem('BconfSVersion', "" + conf.version);
+          setBconfValue("BconfSVersion", "" + conf.version);
           document.location.reload(true);
         })
         this.stop();
@@ -118,7 +198,9 @@ Bconf = {
     return Bconf.baseGet(bKey, Bconf.dataS)
   },
 }
+
 Meteor.startup(function () {
+  initBconfDB();
   Bconf.init();
   Bconf.refresh();
   if (!window.Bconf) {
