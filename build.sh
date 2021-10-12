f20344944c8a9fc8cc0e0ef23cfadc2a58f76e5c
huy nguyen <quochuy@chotot.vn>
2021-10-12 14:12:47 +0700

    chore: update build using yarn

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 run_dir=$(pwd)
 cd $run_dir/packages/platform-modules && npm install --production && node bundle.js
 cd $run_dir
-meteor npm install --production
+meteor yarn install --production
 meteor build /home/meteor/bundle --server-only --directory --allow-incompatible-update
